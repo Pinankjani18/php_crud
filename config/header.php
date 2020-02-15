e75cfa570ff11402928db3b0c98a5425a39fd649
Pinank Jani <34706180+Pinankjani18@users.noreply.github.com>
2020-02-15 12:15:01 +0530

    Initial commit

---
+++ config/header.php
@@ -0,0 +1,11 @@
+<?php 
+	include "connection.php";
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title><?php echo $page_title; ?></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	</head>
+	<body>
